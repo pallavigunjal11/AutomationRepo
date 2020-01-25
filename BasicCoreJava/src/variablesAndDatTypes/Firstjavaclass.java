336a2ab993e569a1ca6f28e8f1940a306659f0e5
Ayushi <mohantyayushi62@gmail.com>
2020-01-25 12:43:29 +0530

    Committing java project

---
+++ BasicCoreJava/src/variablesAndDatTypes/Firstjavaclass.java
@@ -0,0 +1,11 @@
+package variablesAndDatTypes;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome to the world of Automation");
+
+	}
+
+}
